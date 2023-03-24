bcc49fe3c0d5a8567cd645723abf0dd104f9ce0a
Cenk Gürses <cenkgrs@gmail.com>
2023-03-24 11:15:09 +0300

    Drivers List

--- lib/screens/admin/settings_screen.dart
+++ lib/screens/admin/settings_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:crud_app/widgets/bottomNavbar.dart';
 import 'package:crud_app/screens/admin/create_driver_screen.dart';
+import 'package:crud_app/models/Driver.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -10,6 +12,7 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  late Future<List<Driver>> futureDrivers;
   static const String _title = 'Ayarlar';
 
   @override
@@ -37,6 +40,94 @@ class _SettingsScreenState extends State<SettingsScreen> {
     );
   }
 
+  Container getDriverName(driver) {
+    return Container(
+      decoration: const BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(12), topLeft: Radius.circular(12))),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 6,
+            child: Text(
+              driver.name,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  driversList(width) {
+    return FutureBuilder<List<Driver>>(
+        future: futureDrivers,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: Visibility(
+                        visible: true,
+                        child: Center(
+                          // scaffold of the app
+                          child: LoadingAnimationWidget.hexagonDots(
+                            color: Colors.blue,
+                            size: 50,
+                          ),
+                        ))));
+          }
+          if (snapshot.hasError) {
+            return Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: const Visibility(
+                        visible: true,
+                        child: Center(
+                            // scaffold of the app
+                            child: Text('Sürücü Verisi Getirilemedi')))));
+          }
+          List<Driver> drivers = snapshot.data ?? [];
+
+          if (drivers.isEmpty) {
+            return Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: const Visibility(
+                        visible: true,
+                        child: Center(
+                            // scaffold of the app
+                            child: Text('Sürücü Bulunamadı')))));
+          }
+          return ListView.builder(
+              itemCount: drivers.length,
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              physics: const ScrollPhysics(),
+              itemBuilder: (context, index) {
+                Driver driver = drivers[index];
+
+                return Card(
+                    margin: const EdgeInsets.all(10),
+                    child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: <Widget>[
+                            getDriverName(driver),
+                            const SizedBox(height: 50),
+                          ],
+                        )));
+              });
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -59,6 +150,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
             ),
             body: ListView(children: [
               createDriverCard(width),
+              driversList(width),
             ]),
             bottomNavigationBar:
                 const BottomNavbar(userType: 'admin', index: 0)));
